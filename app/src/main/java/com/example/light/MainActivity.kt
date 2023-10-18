215d540be5b4faec4753f985f0a02063233c8733
S.Chanchon <chanchon.splus@gmail.com>
2023-10-18 20:18:30 +0700

    add check permission on bluetooth connection

--- app/src/main/java/com/example/light/MainActivity.kt
+++ app/src/main/java/com/example/light/MainActivity.kt
@@ -1,26 +1,51 @@
 package com.example.light
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
-import java.io.IOException
-
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var bt:BluetoothSocket
     lateinit var deviceName: String
     lateinit var deviceAddress: String
+    lateinit var btAdapter: BluetoothAdapter
+    lateinit var btManager: BluetoothManager
+
     private val STATUS: Int = 1 //Bluetooth connection status
+    private val REQUEST_ENABLE_BT = 200
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (STATUS == 1){
-
+        btManager = getSystemService(BluetoothManager::class.java)
+        btAdapter = btManager.adapter
+        if (btAdapter == null){
+            Log.w("BT ADAPTER","device not support")
         }
+
+        if (btAdapter?.isEnabled == false) {
+            val Intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // if permission is not granted
+                return
+            }
+            startActivityForResult(Intent, REQUEST_ENABLE_BT)
+        }
+
+
     }
 }
