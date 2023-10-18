bff2449b4389fe99d52a39c6c38e5d64875465f1
S.Chanchon <chanchon.splus@gmail.com>
2023-10-18 20:58:01 +0700

    move bluetooth connection into func

--- app/src/main/java/com/example/light/MainActivity.kt
+++ app/src/main/java/com/example/light/MainActivity.kt
@@ -14,26 +14,37 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import android.util.Log
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var bt:BluetoothSocket
+    lateinit var bt: BluetoothSocket
     lateinit var deviceName: String
     lateinit var deviceAddress: String
     lateinit var btAdapter: BluetoothAdapter
     lateinit var btManager: BluetoothManager
+    lateinit var btDevice: BluetoothDevice
 
     private val STATUS: Int = 1 //Bluetooth connection status
     private val REQUEST_ENABLE_BT = 200
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        BluetoothConn()
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        //unregisterReceiver(receiver)
+    }
+
+    fun BluetoothConn() {
+
         btManager = getSystemService(BluetoothManager::class.java)
         btAdapter = btManager.adapter
-        if (btAdapter == null){
-            Log.w("BT ADAPTER","device not support")
+        if (btAdapter == null) {
+            Log.w("BT ADAPTER", "device not support")
         }
 
         if (btAdapter?.isEnabled == false) {
@@ -49,23 +60,33 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(Intent, REQUEST_ENABLE_BT)
         }
 
-        private val receiver = object : BroadcastReceiver() {
+        val receiver = object : BroadcastReceiver() {
 
             override fun onReceive(context: Context, intent: Intent) {
                 val action: String? = intent.action
-                when(action) {
+                when (action) {
                     BluetoothDevice.ACTION_FOUND -> {
                         // Discovery has found a device. Get the BluetoothDevice
                         // object and its info from the Intent.
                         val device: BluetoothDevice? =
                             intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                        val deviceName = device.name
-                        val deviceHardwareAddress = device?.address // MAC address
+                        deviceName = device.name
+                        deviceAddress = device?.address // MAC address
                     }
                 }
             }
         }
         val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, filter)
+
+        val discoverDevicesIntent = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, discoverDevicesIntent)
+        btAdapter.startDiscovery()
+        val uuid: UUID = UUID.fromString("00000001-0000-1000-8000-22305F9B34FB")
+
+        //val device = btAdapter.getRemoteDevice("Device_Address")
+        val socket = btDevice.createRfcommSocketToServiceRecord(uuid)
+
+        socket.connect()
     }
 }
+
