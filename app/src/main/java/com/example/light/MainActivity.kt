c739ecf47e35365e078942488fbb704c76fd054b
Chanchon Saisema <chanchon.splus@gmail.com>
2023-10-24 10:59:48 +0700

    [Main] add NavigationView, Record page and initialized icon

--- app/src/main/java/com/example/light/MainActivity.kt
+++ app/src/main/java/com/example/light/MainActivity.kt
@@ -1,22 +1,39 @@
 package com.example.light
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.light.practice.PracticeFragment
+import com.example.light.record.RecordFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    /*
+        insert Navigation Bar to MainActivity
+    */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn: Button = findViewById(R.id.btn_frag)
-        btn.setOnClickListener {
-            replaceFragment(PracticeFragment())
-            btn.visibility = View.GONE
+//        val btn: Button = findViewById(R.id.btn_frag)
+//        btn.setOnClickListener {
+//            replaceFragment(PracticeFragment())
+//            btn.visibility = View.GONE
+//        }
+        val practiceFragment = PracticeFragment()
+        // val homeFragment=HomeFragment()
+        val recordFragment = RecordFragment()
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        // replaceFragment(practiceFragment)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(practiceFragment)
+                // R.id.person->setCurrentFragment(secondFragment)
+                R.id.record -> replaceFragment(recordFragment)
+            }
+            true
         }
     }
 
--- app/src/main/java/com/example/light/MenuView.kt
+++ app/src/main/java/com/example/light/MenuView.kt
@@ -1,19 +1,36 @@
 package com.example.light
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class MenuView : Fragment() {
-   
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_view, container, false)
-    }
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        // return inflater.inflate(R.layout.fragment_menu_view, container, false)
+//        val practiceFragment = PracticeFragment()
+//        // val homeFragment=HomeFragment()
+//        val recordFragment = RecordFragment()
+//        val bottomNavigationView = container?.findViewById<FrameLayout>(R.id.bottomNavigationView)
+//        setCurrentFragment(practiceFragment)
+//
+//        bottomNavigationView.addOnD(this::onItemSelectedListener) {
+//            when (it.itemId) {
+//                R.id.home -> setCurrentFragment(practiceFragment)
+//                //R.id.person->setCurrentFragment(secondFragment)
+//                R.id.record -> setCurrentFragment(recordFragment)
+//
+//            }
+//            true
+//        }
+//    }
+//
+//        private fun setCurrentFragment(fragment: Fragment) =
+//            supportFragmentManager.beginTransaction().apply {
+//                replace(R.id.flFragment, fragment)
+//                commit()
+//            }
 }
